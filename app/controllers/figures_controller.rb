3772a2a905ac6bcbcaa2cbd44e65227752410b5c
lawrend <douglaslawrence314@gmail.com>
2016-03-29 21:40:21 -0700

    file structure done

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,15 @@
+class FiguresController < ApplicationController
+
+  get '/' do 
+  end
+
+  get 'figures/new' do
+  end
+
+  get '/figures/:id' do 
+  end
+
+  get '/figures/:id/edit' do 
+  end
+
+end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,16 @@
 class LandmarksController < ApplicationController
 
+  get '/' do 
+  end
+
+  get '/landmarks/new' do 
+  end
+
+  get '/landmarks/:id' do
+  end
+
+  get '/landmarks/:id/edit' do 
+  end
+  
+
 end
