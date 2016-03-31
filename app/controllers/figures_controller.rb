6f8492f89dcacb4fe3ee51bd5399888f4c359540
lawrend <douglaslawrence314@gmail.com>
2016-03-30 18:23:59 -0700

    landmarks controllers passing

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,15 +1,19 @@
 class FiguresController < ApplicationController
 
-  get '/' do 
+  get '/figures' do 
+    erb :'/figures/index'
   end
 
   get 'figures/new' do
+    erb :'figures/new'
   end
 
   get '/figures/:id' do 
+    erb :'figures/show'
   end
 
   get '/figures/:id/edit' do 
+    erb :'figures/edit'
   end
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,16 +1,36 @@
 class LandmarksController < ApplicationController
 
-  get '/' do 
+  get '/landmarks' do
+    erb :'/landmarks/index' 
   end
 
   get '/landmarks/new' do 
+    erb :'/landmarks/new'
   end
 
+  post '/landmarks/new' do
+    @landmark = Landmark.new(params[:landmark])
+    @landmark.save
+    redirect '/landmarks'
+  end
+
+
   get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
   end
 
   get '/landmarks/:id/edit' do 
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
   end
-  
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+
 
 end
